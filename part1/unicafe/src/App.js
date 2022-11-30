9baf9ba015bee6e37c787de81f161b1d00588a7d
Tokyo <34824376+ckyx@users.noreply.github.com>
2022-11-30 17:31:01 +0800

    update unicafe app

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,52 +1,63 @@
-import { useState } from "react"
+import { useState } from 'react'
 
-const History = (props) => {
-    if(props.allClicks.length === 0){
+const Header = ({tittle}) => <h1>{tittle}</h1>
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+const Statistics = (props) => {
+
+    const average = () => (props.good + props.neutral + props.bad)/3
+    const all = () => (props.good + props.neutral + props.bad)
+    const positive = () => ((props.good/all())*100.00).toFixed(2) + '%'
+    
+    if(all() === 0){
         return (
             <div>
-                the app is used by pressing the button
+                No feedback given!
             </div>
         )
     }
     return (
         <div>
-            button press history : {props.allClicks.join(' ')}
+        <h2>Statistics</h2>
+        <Display text="good" value={props.good} />
+        <Display text="neutral" value={props.neutral} />
+        <Display text="bad" value={props.bad} />
+        <Display text="all" value={all()}/>
+        <Display text="average" value={average()} />
+        <Display text="positive" value={positive()} />
+        
         </div>
     )
 }
 
+const Display = (props) => <p>{props.text}: {props.value}</p>
+
+
+
+
 const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-    const [clicks, setClicks] = useState({left:0 , right:0})
-    const [allClicks, setAll] = useState([])
+  const increaseGood = () => setGood(good+1)
+  const increaseNeutral = () => setNeutral(neutral+1)
+  const increaseBad = () => setBad(bad+1)
 
-    const leftClick = () => setClicks({...clicks, left: clicks.left + 1})
-    const rightClick = () => setClicks({...clicks, right: clicks.right +1})
-    const handleLeftClick = () => {
-        setAll(allClicks.concat('L'))
-        leftClick()
-    }
+  const tittle = 'Give Feedback'
 
-    const handleRightClick = () => {
-        setAll(allClicks.concat('R'))
-        rightClick()
-    }
+  return (
+    <div>
+        <Header tittle={tittle} />
+        <Button onClick={increaseGood} text='Good'/>
+        <Button onClick={increaseNeutral} text='Neutral'/>
+        <Button onClick={increaseBad} text='Bad'/>
 
-
-    return (
-        <div>
-            {clicks.left}
-            <button onClick={handleLeftClick}>
-                left
-            </button>
-
-            {clicks.right}
-            <button onClick={handleRightClick}>
-                right
-            </button>
-            <History allClicks={allClicks} />
-        </div>
-    )
+        <Statistics good={good} neutral={neutral} bad={bad} />
+       
+      
+    </div>
+  )
 }
 
 export default App
